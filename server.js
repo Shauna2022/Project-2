b94d3e6874aadd2fef17af85f0614adecdb5160b
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-05-28 17:26:02 -0400

    first commit + basic ouline set

---
+++ server.js
@@ -0,0 +1,35 @@
+////////////////////////////////////////////////////////////
+//                      REQUIRE DEPENDENCIES
+////////////////////////////////////////////////////////////
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express()
+require('dotenv').config()
+const mongoose = require('mongoose')
+const productController = require('./controllers/products')
+////////////////////////////////////////////////////////////
+//                      SET UP DATEBASE
+////////////////////////////////////////////////////////////
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	
+});
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+////////////////////////////////////////////////////////////
+//                      MIDDLEWARE
+////////////////////////////////////////////////////////////
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: false }));
+app.use('/product', productController)
+
+////////////////////////////////////////////////////////////
+//                      LISTEN FOR PORT
+////////////////////////////////////////////////////////////
+app.listen(3000, () =>{
+    console.log('Express is listening...')
+})
