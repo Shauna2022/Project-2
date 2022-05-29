883f839829d0db3cc9ff559f56556fd18d7fc5d1
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-05-29 14:47:44 -0400

    added data for seed route + made new view folders

---
+++ models/eyesSeed.js
@@ -0,0 +1,24 @@
+module.exports = [
+    {
+      name: 'Better Than Sex Mascara',
+      brand: 'Too faced',
+      price: 27,
+      color: 'black'
+    }, {
+      name: 'Voluminous Lash Paradise Mascara',
+      brand: "L'Oreal",
+      price: 11.99,
+      color: 'black'
+    }, {
+      name: 'Sunest eye shadow Palette',
+      brand: 'Natasha Denona',
+      price: 129,
+      color: 'shimmer finish'
+    },{
+      name: 'Califoria Love',
+      brand: 'ColourPop',
+      price: 12.60,
+      color: 'Gold and Bronze shimmer finish'
+    },
+]
+
---
+++ models/lipsSeed.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+      name: 'Better Than Sex Mascara',
+      brand: 'Too faced',
+      price: 27,
+      color: 'black'
+    }, {
+      name: 'Voluminous Lash Paradise Mascara',
+      brand: "L'Oreal",
+      price: 11.99,
+      color: 'black'
+    }, {
+      name: 'Bins',
+      brand: 'A stack of colorful bins for your beans and bones.',
+      price: 7000,
+      color: 1
+    },{
+      name: '',
+      brand: '',
+      price: ,
+      color: ''
+    },
+]
