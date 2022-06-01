bc83759fc1d44c26dd0b0fc7d715fac7a3165950
KriShauna Fisher <Krishaunafisher@gmail.com>
2022-06-01 11:31:36 -0400

    setting up routes for new edit and delete

--- controllers/productRoutes/lips.js
+++ controllers/productRoutes/lips.js
@@ -19,6 +19,42 @@ router.get('/seed', (req, res) => {
 		res.redirect('/dupe');
 	});
 });
+//////////////////////// DELETE ////////////////////////
+router.delete('/:id', (req, res) => {
+    Product.findByIdAndDelete(req.params.id, (error, data) => {
+        res.redirect('/duped/:id')
+    })
+})
+
+//////////////////////// UPDATE ////////////////////////
+router.put('/:id', (req, res) => {
+    Product.findByIdAndUpdate(req.params.id, req.body,{
+        new: true
+    }, (error, updatedProduct) => {
+        res.redirect('/duped/:id')
+    })
+})
+
+//////////////////////// CREATE ////////////////////////
+router.post('/', (req, res) => {
+    Product.create(req.body, (error, createdProduct) => {
+        res.redirect('/duped')
+    })
+})
+
+//////////////////////// EDIT ////////////////////////
+router.get('/:id/edit', (req, res) => {
+    Product.findById(req.params.id, (error, foundProduct) =>{
+        res.render('edit.ejs', {
+            product: foundProduct
+        })
+    })
+})
+
+//////////////////////// NEW ////////////////////////
+router.get('/new', (req, res) => {
+    res.render('lips/new.ejs')
+})
 
 //////////////////////// SHOW ////////////////////////////
 router.get('/:id', (req, res) =>{
